6a7c05979cddc7c5a11cdb592a2cc072356a12c5
tonivarga9 <tonivarga2005@gmail.com>
2025-05-12 18:44:37 +0200

    Add files via upload

---
+++ Završni Rad - Programiranje 2/Header.h
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAKS_NAZIV 50
+#define DATOTEKA "skladiste_douglas.dat"
+
+// ENUMI
+typedef enum {
+    OPCIJA_DODAJ = 1,
+    OPCIJA_PREGLED,
+    OPCIJA_AZURIRAJ,
+    OPCIJA_OBRISI,
+    OPCIJA_SORTIRAJ,
+    OPCIJA_PRETRAZI,
+    OPCIJA_IZLAZ
+} IzbornikOpcija;
+
+typedef enum {
+    STATUS_AKTIVAN = 1,
+    STATUS_OBRISAN = 0
+} StatusParfem;
+
+// STRUKTURA
+typedef struct {
+    int sifra;
+    char naziv[MAKS_NAZIV];
+    int kolicina;
+    float cijena;
+    StatusParfem status;
+} Parfem;
+
+// GLOBALNE PROMJENJIVE
+extern Parfem* parfemi;
+extern size_t brojParfema;
+
+// DEKLARACIJE FUNKCIJA
+void prikaziIzbornik();
+void dodajParfem();
+void prikaziParfeme();
+void azurirajParfem();
+void obrisiParfem();
+void sortirajParfeme();
+void pretraziParfem();
+void ucitajIzDatoteke();
+void spremiUDatoteku();
+void oslobodiMemoriju();
+
+int usporediNaziv(const void*, const void*);
+int usporediSifre(const void*, const void*);
+Parfem* traziParfemPoSifri(int sifra);
+
+#endif
+
+
---
+++ Završni Rad - Programiranje 2/main.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include "header.h"
+
+int main() {
+    int izbor;
+    ucitajIzDatoteke();
+
+    do {
+        prikaziIzbornik();
+        scanf("%d", &izbor);
+
+        switch (izbor) {
+        case OPCIJA_DODAJ: dodajParfem(); break;
+        case OPCIJA_PREGLED: prikaziParfeme(); break;
+        case OPCIJA_AZURIRAJ: azurirajParfem(); break;
+        case OPCIJA_OBRISI: obrisiParfem(); break;
+        case OPCIJA_SORTIRAJ: sortirajParfeme(); break;
+        case OPCIJA_PRETRAZI: pretraziParfem(); break;
+        case OPCIJA_IZLAZ: oslobodiMemoriju(); break;
+        default: printf("Nepoznata opcija.\n"); break;
+        }
+
+    } while (izbor != OPCIJA_IZLAZ);
+
+    return 0;
+}
